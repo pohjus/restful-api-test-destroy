19af0037c560423357fd13c8cbe13f097b02be98
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2023-01-02 15:08:22 +0200

    Initial project

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// serve the public directory as a static content here
+app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Example app listeningd at http://localhost:${port}`);
+});
