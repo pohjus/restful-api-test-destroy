57f61ce1a2a1801e5f19ff01402a58c5b7cead0a
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2023-01-02 15:23:18 +0200

    FIX port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // serve the public directory as a static content here
 app.use(express.static("public"));
